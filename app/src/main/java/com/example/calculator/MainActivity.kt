db78b54e3c65100d67030ffe947eb501193b3b6b
Zaah91 <zaah91@gmail.com>
2023-02-10 12:59:12 +0100

    calculator whit spinner - functional

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,17 +17,30 @@ class MainActivity : AppCompatActivity() {
         val view: View = binding.root
         setContentView(view)
 
-        binding.buttonAddNumbers.setOnClickListener {
+        val spinner : Spinner = binding.calculatorSpinner
+        ArrayAdapter.createFromResource(
+            this, R.array.calculatorOperators, android.R.layout.simple_spinner_dropdown_item
+        ).also {adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
 
+        binding.buttonCalculateNumbers.setOnClickListener {
             var firstNumber = binding.numberOne.text.toString().toInt()
             var secondNumber = binding.numberTwo.text.toString().toInt()
+            val operation = binding.calculatorSpinner.selectedItem
+            var result = 0.0
 
-            var result = firstNumber + secondNumber
+            when (operation){
+                "+" -> result = (firstNumber + secondNumber).toDouble()
+                "-" -> result = (firstNumber - secondNumber).toDouble()
+                "/" -> result = (firstNumber / secondNumber).toDouble()
+                "*" -> result = (firstNumber * secondNumber).toDouble()
+            }
+
             binding.mathResult.text = "$result"
 
         }
-
-
 
     }
 }
